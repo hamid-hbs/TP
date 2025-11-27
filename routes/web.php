be4f11d9fd4d51b198969b68c06f71f6afa82cd0
john230624 <jbladeleboladji@gmail.com>
2025-11-27 12:51:38 +0100

    Clonage terminer avec succès

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Route as RouteFacade;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +18,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+//Mise à jour du commit d'Alex
