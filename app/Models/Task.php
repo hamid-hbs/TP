f531975c562637e55b1b912483e9d5bdd7e3f27a
COCOUVI ALEXANDRO <cocouvialexandro74@gmail.com>
2025-11-27 12:15:16 +0100

    Creation de la BDD

---
+++ app/Models/Task.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    //
+    protected $fillable = [
+        'title',
+        'description',
+        'is_completed',
+        'due_date',
+        'user_id',
+    ];
+}
